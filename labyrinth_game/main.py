b1cf1b002b8230aef23233f97ef06ac9e5192e94
Arina <79160367+arinaat@users.noreply.github.com>
2026-01-13 19:12:14 +0300

    Скелет игрового цикла +Исправление ошибки имени файла

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,24 @@
+from labyrinth_game.utils import describe_current_room
+
+
 def main() -> None:
-    print("Первая попытка запустить проект")
+    game_state = {
+        "player_inventory": [],
+        "current_room": "entrance",
+        "game_over": False,
+        "steps_taken": 0,
+    }
+
+    print("Добро пожаловать в Лабиринт сокровищ!")
+    describe_current_room(game_state)
+
+    while not game_state["game_over"]:
+        command = input("> ").strip().lower()
+        if command in ("quit", "exit"):
+            game_state["game_over"] = True
+            print("Вы вышли из игры.")
+        else:
+            print("Пока доступны только команды: quit / exit")
 
 
 if __name__ == "__main__":
--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -1,1 +1,8 @@
-# Вспомогательные функции
+from labyrinth_game.constants import ROOMS
+
+
+def describe_current_room(game_state: dict) -> None:
+    room_name = game_state["current_room"]
+    room = ROOMS[room_name]
+    print(f"\n== {room_name.upper()} ==")
+    print(room["description"])
